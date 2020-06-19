60f84726d5af43c7c17c00790f72ffbcf200996e
Manuel <manuel.serrano943@gmail.com>
2020-06-19 17:17:44 +0200

    Corrección de errores por commits

--- app/src/main/java/com/example/manue/elgourmet/Util/Receta.java
+++ app/src/main/java/com/example/manue/elgourmet/Util/Receta.java
@@ -7,7 +7,6 @@ public class Receta implements Serializable {
 
     private String imagenUrl;
     private String nombreReceta;
-<<<<<<< HEAD
     private int tiempo;
     private int porciones;
     private String url;
@@ -15,49 +14,31 @@ public class Receta implements Serializable {
     public Receta(String imagenUrl, String nombreReceta, int tiempo, int porciones, String url) {
         this.imagenUrl = imagenUrl;
         this.nombreReceta = nombreReceta;
-        this.tiempo= tiempo;
+        this.tiempo = tiempo;
         this.porciones = porciones;
-=======
-    private String instruccionesReceta;
-    private String[] ingredientes;
-    private String url;
-
-    public Receta(String imagenUrl, String nombreReceta, String instruccionesReceta, String[] ingredientes, String url) {
-        this.imagenUrl = imagenUrl;
-        this.nombreReceta = nombreReceta;
-        this.instruccionesReceta = instruccionesReceta;
-        this.ingredientes = ingredientes;
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
         this.url = url;
     }
+
 
     public String getUrl() {
         return url;
     }
-
-<<<<<<< HEAD
-=======
     public void setUrl(String url) {
         this.url = url;
     }
 
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
     public String getImagenUrl() {
         return imagenUrl;
     }
 
-<<<<<<< HEAD
-=======
     public void setImagenUrl(String imagenUrl) {
         this.imagenUrl = imagenUrl;
     }
 
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
     public String getNombreReceta() {
         return nombreReceta;
     }
 
-<<<<<<< HEAD
     public int getTiempo() {
         return tiempo;
     }
@@ -65,26 +46,7 @@ public class Receta implements Serializable {
     public int getPorciones() {
         return porciones;
     }
-=======
     public void setNombreReceta(String nombreReceta) {
         this.nombreReceta = nombreReceta;
     }
-
-    public String getInstruccionesReceta() {
-        return instruccionesReceta;
-    }
-
-    public void setInstruccionesReceta(String instruccionesReceta) {
-        this.instruccionesReceta = instruccionesReceta;
-    }
-
-    public String[] getIngredientes() {
-        return ingredientes;
-    }
-
-    public void setIngredientes(String[] ingredientes) {
-        this.ingredientes = ingredientes;
-    }
-
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
 }
--- app/src/main/java/com/example/manue/elgourmet/fragments/RecetasFragment.java
+++ app/src/main/java/com/example/manue/elgourmet/fragments/RecetasFragment.java
@@ -50,13 +50,9 @@ public class RecetasFragment extends Fragment {
     private FloatingActionButton back;
 
 
-<<<<<<< HEAD
     private static final String api = "https://api.spoonacular.com/recipes/search?&apiKey=81b0fa8b8d5841c992c3df7c2c570ec0";
     private static final String random = "https://api.spoonacular.com/recipes/search?&apiKey=81b0fa8b8d5841c992c3df7c2c570ec0";
-=======
-    private static final String api = "https://www.themealdb.com/api/json/v1/1/latest.php";
-    private static final String random = "https://www.themealdb.com/api/json/v1/1/random.php";
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -125,22 +121,17 @@ public class RecetasFragment extends Fragment {
         dialog= builder.create();
         dialog.show();
 
-<<<<<<< HEAD
+
         final EditText porciones_input = view.findViewById(R.id.ingrediente);
-=======
-        final EditText ingr = view.findViewById(R.id.ingrediente);
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
+
         Button botonFiltrar = view.findViewById(R.id.popupFiltrar);
         Button mostrarTodo = view.findViewById(R.id.mostrarTodo);
         botonFiltrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-<<<<<<< HEAD
+
                 getDataFilter(Integer.parseInt(porciones_input.getText().toString()));
-=======
-                getDataFilter(ingr.getText().toString());
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
 
                 dialog.dismiss();
             }
@@ -166,7 +157,6 @@ public class RecetasFragment extends Fragment {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-<<<<<<< HEAD
                             JSONArray jsonArray = response.getJSONArray("results");
 
                             for(int i = 0; i < jsonArray.length() ; i++){
@@ -182,49 +172,6 @@ public class RecetasFragment extends Fragment {
                                 recetaLista.add(r2);
                             }
 
-=======
-                            JSONArray jsonArray = response.getJSONArray("meals");
-
-                            for(int i = 0; i < jsonArray.length() ; i++){
-                                JSONObject receta = jsonArray.getJSONObject(i);
-                                String[] ingredientes = new String[20];
-                                String nombreReceta = receta.getString("strMeal");
-
-                                String imagenUrl = receta.getString("strMealThumb");
-
-                                ingredientes[0] = receta.getString("strIngredient1");
-                                ingredientes[1] = receta.getString("strIngredient2");
-                                ingredientes[2] = receta.getString("strIngredient3");
-                                ingredientes[3] = receta.getString("strIngredient4");
-                                ingredientes[4] = receta.getString("strIngredient5");
-                                ingredientes[5] = receta.getString("strIngredient6");
-                                ingredientes[6] = receta.getString("strIngredient7");
-                                ingredientes[7] = receta.getString("strIngredient8");
-                                ingredientes[8] = receta.getString("strIngredient9");
-                                ingredientes[9] = receta.getString("strIngredient10");
-                                ingredientes[10] = receta.getString("strIngredient11");
-                                ingredientes[11] = receta.getString("strIngredient12");
-                                ingredientes[12] = receta.getString("strIngredient13");
-                                ingredientes[13] = receta.getString("strIngredient14");
-                                ingredientes[14] = receta.getString("strIngredient15");
-                                ingredientes[15] = receta.getString("strIngredient16");
-                                ingredientes[16] = receta.getString("strIngredient17");
-                                ingredientes[17] = receta.getString("strIngredient18");
-                                ingredientes[18] = receta.getString("strIngredient19");
-                                ingredientes[19] = receta.getString("strIngredient20");
-
-                                String elaboracion = receta.getString("strInstructions");
-
-                                String url = receta.getString("strSource");
-
-
-
-                                Receta r2 = new Receta(imagenUrl, nombreReceta, elaboracion, ingredientes, url);
-                                recetaLista.add(r2);
-
-
-                            }
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
                             adapter = new RecetasAdapter(recetaLista, getActivity().getApplicationContext(), new RecetasAdapter.OnItemClickListener() {
                                 @Override
                                 public void onItemClick(Receta receta, int position) {
@@ -247,11 +194,7 @@ public class RecetasFragment extends Fragment {
         mRequestQueue.add(request);
     }
 
-<<<<<<< HEAD
     private void getDataFilter(final int porciones_input) {
-=======
-    private void getDataFilter(final String ingrediente) {
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
         recetaLista.clear();
         mRequestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, api, null,
@@ -259,7 +202,6 @@ public class RecetasFragment extends Fragment {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-<<<<<<< HEAD
                             JSONArray jsonArray = response.getJSONArray("results");
 
                             for(int i = 0; i < jsonArray.length() ; i++){
@@ -276,60 +218,6 @@ public class RecetasFragment extends Fragment {
                                     recetaLista.add(r2);
                                 }
 
-=======
-                            JSONArray jsonArray = response.getJSONArray("meals");
-
-                            for(int i = 0; i < jsonArray.length() ; i++){
-                                JSONObject receta = jsonArray.getJSONObject(i);
-                                String[] ingredientes = new String[20];
-                                String nombreReceta = receta.getString("strMeal");
-                                String imagenUrl = receta.getString("strMealThumb");
-
-                                ingredientes[0] = receta.getString("strIngredient1");
-                                ingredientes[1] = receta.getString("strIngredient2");
-                                ingredientes[2] = receta.getString("strIngredient3");
-                                ingredientes[3] = receta.getString("strIngredient4");
-                                ingredientes[4] = receta.getString("strIngredient5");
-                                ingredientes[5] = receta.getString("strIngredient6");
-                                ingredientes[6] = receta.getString("strIngredient7");
-                                ingredientes[7] = receta.getString("strIngredient8");
-                                ingredientes[8] = receta.getString("strIngredient9");
-                                ingredientes[9] = receta.getString("strIngredient10");
-                                ingredientes[10] = receta.getString("strIngredient11");
-                                ingredientes[11] = receta.getString("strIngredient12");
-                                ingredientes[12] = receta.getString("strIngredient13");
-                                ingredientes[13] = receta.getString("strIngredient14");
-                                ingredientes[14] = receta.getString("strIngredient15");
-                                ingredientes[15] = receta.getString("strIngredient16");
-                                ingredientes[16] = receta.getString("strIngredient17");
-                                ingredientes[17] = receta.getString("strIngredient18");
-                                ingredientes[18] = receta.getString("strIngredient19");
-                                ingredientes[19] = receta.getString("strIngredient20");
-
-                                String elaboracion = receta.getString("strInstructions");
-
-                                String url = receta.getString("strSource");
-
-                                boolean filtrar = false;
-                                for(int j=0;j<20;j++){
-                                    if(ingredientes[j].equals(ingrediente)){
-                                        filtrar = true;
-                                        break;
-                                    }else{
-                                        filtrar = false;
-                                    }
-
-                                }
-
-
-                                Receta r2 = new Receta(imagenUrl, nombreReceta, elaboracion, ingredientes, url);
-                                if(filtrar){
-                                    recetaLista.add(r2);
-                                }
-
-
-
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
                             }
 
                             adapter2 = new RecetasFiltrarAdapter(recetaLista, getActivity().getApplicationContext(), new RecetasFiltrarAdapter.OnItemClickListener() {
@@ -365,7 +253,6 @@ public class RecetasFragment extends Fragment {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-<<<<<<< HEAD
                             JSONArray jsonArray = response.getJSONArray("results");
 
                             for(int i = 0; i < jsonArray.length() ; i++){
@@ -381,51 +268,7 @@ public class RecetasFragment extends Fragment {
                                     recetaLista.add(r2);
 
                             }
-=======
-                            JSONArray jsonArray = response.getJSONArray("meals");
 
-                            for(int i = 0; i < jsonArray.length() ; i++){
-                                JSONObject receta = jsonArray.getJSONObject(i);
-                                String[] ingredientes = new String[20];
-                                String nombreReceta = receta.getString("strMeal");
-                                String imagenUrl = receta.getString("strMealThumb");
-
-                                ingredientes[0] = receta.getString("strIngredient1");
-                                ingredientes[1] = receta.getString("strIngredient2");
-                                ingredientes[2] = receta.getString("strIngredient3");
-                                ingredientes[3] = receta.getString("strIngredient4");
-                                ingredientes[4] = receta.getString("strIngredient5");
-                                ingredientes[5] = receta.getString("strIngredient6");
-                                ingredientes[6] = receta.getString("strIngredient7");
-                                ingredientes[7] = receta.getString("strIngredient8");
-                                ingredientes[8] = receta.getString("strIngredient9");
-                                ingredientes[9] = receta.getString("strIngredient10");
-                                ingredientes[10] = receta.getString("strIngredient11");
-                                ingredientes[11] = receta.getString("strIngredient12");
-                                ingredientes[12] = receta.getString("strIngredient13");
-                                ingredientes[13] = receta.getString("strIngredient14");
-                                ingredientes[14] = receta.getString("strIngredient15");
-                                ingredientes[15] = receta.getString("strIngredient16");
-                                ingredientes[16] = receta.getString("strIngredient17");
-                                ingredientes[17] = receta.getString("strIngredient18");
-                                ingredientes[18] = receta.getString("strIngredient19");
-                                ingredientes[19] = receta.getString("strIngredient20");
-
-                                String elaboracion = receta.getString("strInstructions");
-
-                                String url = receta.getString("strSource");
-
-
-
-                                Receta r2 = new Receta(imagenUrl, nombreReceta, elaboracion, ingredientes, url);
-                                    recetaLista.add(r2);
-
-
-
-
-                            }
-
->>>>>>> 2d84e4012000c2234d28842114a8444d39e00c1b
                             adapter2 = new RecetasFiltrarAdapter(recetaLista, getActivity().getApplicationContext(), new RecetasFiltrarAdapter.OnItemClickListener() {
                                 @Override
                                 public void onItemClick(Receta receta, int position) {
